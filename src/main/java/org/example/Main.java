6fa86a6fba0d8e69588b5fcea1dcd18fc89beb64
luqmansolihin <luqmansolihin.jte@gmail.com>
2022-01-24 20:10:54 +0700

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.logging.Logger;
+
+public class Main {
+    static final Logger logger = Logger.getLogger("Main.class");
+    
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+    }
+}
